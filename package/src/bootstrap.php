939e307a67299b74cba2e5e12d2604083e7e4532
Stephen Lake <stephenlake@users.noreply.github.com>
2018-04-19 20:55:37 +0200

    Update bootstrap.php

--- package/src/bootstrap.php
+++ package/src/bootstrap.php
@@ -4,7 +4,7 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 
 function addpay($client_id, $client_secret, $live)
 {
-    $client = \AddPay\Foundation\Api::client($client_id, $client_secret, $live);
+    $client = new \AddPay\Foundation\Api($client_id, $client_secret, $live);
 
     return $client->prepare();
 }
